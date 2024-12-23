201ddd92c563ebd2c9cb30f1955ebe8bf714e2db
Bokkor <abub33690@gmail.com>
2024-12-24 04:24:35 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/teachdb')
